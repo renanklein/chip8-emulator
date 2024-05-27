c3e38331c5cb7c9110e8e4e9948e4514c4a80479
renanklein <renankleinshow@gmail.com>
2024-05-27 09:41:23 -0300

    Initialize screen module

--- internal/chip8/screen.go
+++ internal/chip8/screen.go
@@ -7,10 +7,29 @@ type Screen struct {
 	pixels [][]int
 }
 
+func Initialize(height int, width int, scale int) Screen {
+	screen := Screen{}
+
+	screen.width = width
+	screen.height = height
+	screen.scale = scale
+	screen.pixels = make([][]int, height)
+
+	for i := 0; i < width; i++ {
+		screen.pixels[i] = make([]int, screen.width)
+	}
+
+	return screen
+}
+
 func (screen *Screen) Clear() {
 	for y := 0; y < screen.height; y++ {
 		for x := 0; x < screen.width; x++ {
 			screen.pixels[x][y] = 0
 		}
 	}
+}
+
+func (screen *Screen) Render() {
+
 }
